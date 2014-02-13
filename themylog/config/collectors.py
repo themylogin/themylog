70e657813df60104e752b88a535b268ea05c40be
themylogin <themylogin@gmail.com>
2014-02-13 18:55:47 +0700

    config.find_scripts

--- themylog/config/collectors.py
+++ themylog/config/collectors.py
@@ -1,30 +1,11 @@
 # -*- coding=utf-8 -*-
 from __future__ import absolute_import, division, unicode_literals
 
-from collections import namedtuple
-import os
-
-from themylog.annotations import read_annotations
 from themylog.annotations.schedule import schedule
-
-Collector = namedtuple("Collector", ["name", "path", "annotations"])
+from themylog.config.scripts import find_scripts
 
 
 def get_collectors(config):
-    collectors = []
-    config_collectors = config.get("collectors")
-    if config_collectors:
-        for collector in os.listdir(config_collectors["directory"]):
-            path = os.path.join(config_collectors["directory"], collector)
-            name, ext = os.path.splitext(collector)
-
-            if ext != ".py":
-                continue
-
-            annotations = read_annotations(open(path).read().decode("utf-8"), {
-                "schedule":     schedule,
-            })
-
-            collectors.append(Collector(name=name, path=path, annotations=annotations))
-
-    return collectors
+    return find_scripts(config.get("collectors", {}).get("directory"), {
+        "schedule":     schedule,
+    })
---
+++ themylog/config/scripts.py
@@ -0,0 +1,26 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, division, unicode_literals
+
+from collections import namedtuple
+import os
+
+from themylog.annotations import read_annotations
+
+Script = namedtuple("Collector", ["name", "path", "annotations"])
+
+
+def find_scripts(directory, annotation_parsers):
+    scripts = []
+    if directory:
+        for script in os.listdir(directory):
+            path = os.path.join(directory, script)
+            name, ext = os.path.splitext(path)
+
+            if ext != ".py":
+                continue
+
+            annotations = read_annotations(open(path).read().decode("utf-8"), annotation_parsers)
+
+            scripts.append(Script(name=name, path=path, annotations=annotations))
+
+    return scripts
