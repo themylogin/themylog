6a71c5e0d1e6801900fb1c64aa7693584bea8810
themylogin <themylogin@gmail.com>
2015-11-17 18:04:23 +0600

    Fix Sentry

--- themylog/handler/sentry.py
+++ themylog/handler/sentry.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 class Sentry(BaseHandler):
     implements(IHandler)
 
-    def __init__(self, team, organization, owner, rules_tree):
+    def __init__(self, team, organization, rules_tree):
         config_file = os.path.expanduser("~/.sentry/sentry.conf.py")
         config = {b"__file__": config_file}
         execfile(config_file, config)
@@ -30,13 +30,11 @@ class Sentry(BaseHandler):
                               if k in ["DATABASES"] or any(k.startswith("%s_" % s)
                                                            for s in ["AUTH", "SENTRY"])})
 
-        from sentry.models import Team, Project, ProjectKey, Organization, User
+        from sentry.models import Team, Project, Organization
         self.Project = Project
-        self.ProjectKey = ProjectKey
 
         self.team = Team.objects.get(name=team)
         self.organization = Organization.objects.get(name=organization)
-        self.owner = User.objects.get(username=owner)
         self.projects = {}
 
         self.rules_tree = get_rules_tree(rules_tree)
@@ -61,7 +59,7 @@ class Sentry(BaseHandler):
             if dsn is None:
                 project = self.Project.objects.get_or_create(team=self.team, organization=self.organization,
                                                              name=record.application)[0]
-                dsn = project.key_set.get_or_create(user=self.owner)[0].get_dsn()
+                dsn = project.key_set.get_or_create()[0].get_dsn()
                 self.projects[record.application] = dsn
 
             client = Client(dsn)
